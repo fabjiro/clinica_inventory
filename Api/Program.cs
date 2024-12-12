d1618c5309b4dbe8daff5353b509c6108be40fd7
fabjiro <fjiron2001@gmail.com>
2024-12-12 10:25:58 -0600

    some chanes

--- Api/Program.cs
+++ Api/Program.cs
@@ -45,11 +45,8 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 {
     // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
     app.UseAuthentication();
     app.UseAuthorization();
