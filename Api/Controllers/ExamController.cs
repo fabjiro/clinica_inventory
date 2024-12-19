7a6659e596c6e772ba46e40d8c0f77327db69bf7
fabjiro <fjiron2001@gmail.com>
2024-12-18 19:57:42 -0600

    add CreateExam endpoint and request DTO for exam creation

--- Api/Controllers/ExamController.cs
+++ Api/Controllers/ExamController.cs
@@ -1,6 +1,11 @@
+using Application.Commands.Exam;
+using Application.Dto.Request.Exam;
 using Application.Dto.Response.Exam;
+using Application.Helpers;
 using Application.Queries.Exam;
+using Ardalis.Result;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
@@ -15,9 +20,33 @@ public class ExamController : ControllerBase
     }
 
     [HttpGet]
+    [Authorize]
     public async Task<ActionResult<List<ExamDto>>> GetAll()
     {
         var exams = await _mediator.Send(new GetAllExamQuery());
         return Ok(exams);
+    }
+
+    [HttpPost]
+    [Authorize]
+    public async Task<ActionResult<ExamDto>> Create([FromBody] CreateExamDtoReq dto)
+    {
+        try
+        {
+            var command = new CreateExamCommand(dto.Name, Guid.Parse(dto.Group));
+            var exam = await _mediator.Send(command);
+
+            if (exam.IsInvalid())
+            {
+                var invalidError = ErrorHelper.GetValidationErrors(exam.ValidationErrors.ToList());
+                return Problem(invalidError, null, 400);
+            }
+
+            return Ok(exam.Value);
+        }
+        catch (Exception ex)
+        {
+            return Problem(ErrorHelper.GetExceptionError(ex));
+        }
     }
 }
