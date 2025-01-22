e8d3e2402e75c346e0975b0250e9f1ecbc3cca65
fabjiro <fjiron2001@gmail.com>
2025-01-22 16:54:05 -0600

    feat: implement report generation for consult registrations by user with date filtering

--- Application/Mapper/Consult/ConsultMapper.cs
+++ Application/Mapper/Consult/ConsultMapper.cs
@@ -1,4 +1,5 @@
 using Application.Dto.Response.Consult;
+using Application.Dto.Response.Report;
 using AutoMapper;
 
 namespace Application.Mapper.Consult;
@@ -8,5 +9,10 @@ public class ConsultMapper : Profile
     public ConsultMapper()
     {
         CreateMap<ConsultEntity, ConsultDtoRes>();
+
+        CreateMap<ConsultEntity, ReportRegistrationByUserResDto>()
+            .ForMember(dest => dest.PatientName, opt => opt.MapFrom(src => src.Patient!.Name))
+            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserCreatedBy!.Name))
+            .ForMember(dest => dest.Diagnostic, opt => opt.MapFrom(src => src.Diagnosis));
     }
 }
