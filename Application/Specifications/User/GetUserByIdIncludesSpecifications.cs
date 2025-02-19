47bfe4dbd315105376ebcc3c0ecf0f408425c629
Fabian Jiron <fjiron2001@gmail.com>
2025-02-19 14:01:18 -0600

    feat: include Rol property in GetUserByIdIncludesSpecifications query

--- Application/Specifications/User/GetUserByIdIncludesSpecifications.cs
+++ Application/Specifications/User/GetUserByIdIncludesSpecifications.cs
@@ -9,6 +9,7 @@ public class GetUserByIdIncludesSpecifications : SingleResultSpecification<UserE
     {
         Query.Where(x => x.Id == id);
         Query.Include(x => x.Rol);
+        Query.Include(x => x.Rol!.Rol);
         Query.Include(x => x.Avatar);
     }
 }
