b6c93d6ea8e5d46035991a08593c11381a2d4016
git config --global user.name Jorge <eduardotreminio10.com>
2025-03-27 13:26:26 -0600

    backups not local because have a error the last commit

--- Application/Handlers/Backup/CrateBackupCommandHandler.cs
+++ Application/Handlers/Backup/CrateBackupCommandHandler.cs
@@ -1,3 +1,4 @@
+
 using Application.Commands.Backup;
 using Application.Dto.Response.Backup;
 using Application.Helpers;
@@ -8,68 +9,67 @@ using Domain.Interface;
 using Domain.Repository;
 using MediatR;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
 
-namespace Application.Handlers.Backup
+namespace Application.Handlers.Backup;
+
+public class CrateBackupCommandHandler : IRequestHandler<CreateBackupCommand, Result<BackupResDto>>
 {
-    public class CrateBackupCommandHandler : IRequestHandler<CreateBackupCommand, Result<BackupResDto>>
+    private readonly IMapper _mapper;
+    private readonly IAsyncRepository<BackupEntity> _backupRepository;
+    private readonly IUploaderRepository _uploaderRepository;
+    private readonly string _connectionString;
+
+    public CrateBackupCommandHandler(IMapper mapper, IAsyncRepository<BackupEntity> backupRepository, IUploaderRepository uploaderRepository, IConfiguration configuration)
     {
-        private readonly IMapper _mapper;
-        private readonly IAsyncRepository<BackupEntity> _backupRepository;
-        private readonly IUploaderRepository _uploaderRepository;
-        private readonly string _connectionString;
+        _mapper = mapper;
+        _backupRepository = backupRepository;
+        _uploaderRepository = uploaderRepository;
+        _connectionString = configuration.GetConnectionString("Default") ?? "";
 
-        public CrateBackupCommandHandler(IMapper mapper, IAsyncRepository<BackupEntity> backupRepository, IUploaderRepository uploaderRepository, IConfiguration configuration)
+    }
+
+    public async Task<Result<BackupResDto>> Handle(CreateBackupCommand request, CancellationToken cancellationToken)
+    {
+        try
         {
-            _mapper = mapper;
-            _backupRepository = backupRepository;
-            _uploaderRepository = uploaderRepository;
-            _connectionString = configuration.GetConnectionString("Default") ?? "";
+            // get temp path
+            var tempPath = Path.GetTempPath();
+            var fileName = "database-backup-" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".dump";
+            var pathToSave = Path.Combine(tempPath, fileName);
+
+            var resutl = await DatabaseHelper.BackupDatabase(_connectionString, pathToSave);
+
+            if (resutl is false)
+            {
+                return Result<BackupResDto>.Invalid(new List<ValidationError> {
+                new () {ErrorMessage = "Backup not created",}
+            });
+            }
+
+            var base64String = Convert.ToBase64String(File.ReadAllBytes(pathToSave));
+
+
+            var upFile = await _uploaderRepository.Upload("data:application/dump;base64," + base64String);
+
+            if (upFile is null)
+            {
+                return Result<BackupResDto>.Invalid(new List<ValidationError> {
+                new () {ErrorMessage = "File not uploaded",}
+            });
+            }
+
+            var backupEntity = await _backupRepository.AddAsync(new BackupEntity(
+                fileName,
+                upFile
+            ), cancellationToken);
+
+            File.Delete(pathToSave);
+
+            return Result.Success(_mapper.Map<BackupResDto>(backupEntity));
         }
-
-        public async Task<Result<BackupResDto>> Handle(CreateBackupCommand request, CancellationToken cancellationToken)
+        catch (Exception ex)
         {
-            try
-            {
-                // Ruta de la carpeta C:\backups
-                var backupsPath = Path.Combine("C:", "backups");
-
-                // Verificar si la carpeta "backups" existe, si no, crearla
-                if (!Directory.Exists(backupsPath))
-                {
-                    Directory.CreateDirectory(backupsPath);
-                }
-
-                var fileName = "database-backup-" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".dump";
-                var pathToSave = Path.Combine(backupsPath, fileName);
-
-                // Generar el backup de la base de datos
-                var result = await DatabaseHelper.BackupDatabase(_connectionString, pathToSave);
-
-                if (!result)
-                {
-                    return Result<BackupResDto>.Invalid(new List<ValidationError> {
-                        new ValidationError { ErrorMessage = "Backup not created" }
-                    });
-                }
-
-                // Aquí ya no necesitamos subir el archivo a un servidor remoto
-                // Así que no hace falta convertir a base64 ni subirlo
-
-                // Guardar la información del backup en la base de datos
-                var backupEntity = await _backupRepository.AddAsync(new BackupEntity(fileName, pathToSave), cancellationToken);
-
-                // Devolver el resultado del backup
-                return Result.Success(_mapper.Map<BackupResDto>(backupEntity));
-            }
-            catch (Exception ex)
-            {
-                return Result.Error(ErrorHelper.GetExceptionError(ex));
-            }
+            return Result.Error(ErrorHelper.GetExceptionError(ex));
         }
     }
 }
--- Application/Handlers/Backup/RestoreBackupCommandHandler.cs
+++ Application/Handlers/Backup/RestoreBackupCommandHandler.cs
@@ -6,61 +6,59 @@ using Domain.Interface;
 using Domain.Repository;
 using MediatR;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
 
-namespace Application.Handlers.Backup
+namespace Application.Handlers.Backup;
+
+public class RestoreBackupCommandHandler : IRequestHandler<RestoreBackupCommand, Result<BackupEntity>>
 {
-    public class RestoreBackupCommandHandler : IRequestHandler<RestoreBackupCommand, Result<BackupEntity>>
+    private readonly IAsyncRepository<BackupEntity> _backupRepository;
+    private readonly string _connectionString;
+
+    public RestoreBackupCommandHandler(IAsyncRepository<BackupEntity> backupRepository, IConfiguration configuration)
     {
-        private readonly IAsyncRepository<BackupEntity> _backupRepository;
-        private readonly string _connectionString;
+        _backupRepository = backupRepository;
+        _connectionString = configuration.GetConnectionString("Default") ?? "";
+    }
 
-        public RestoreBackupCommandHandler(IAsyncRepository<BackupEntity> backupRepository, IConfiguration configuration)
+    public async Task<Result<BackupEntity>> Handle(RestoreBackupCommand request, CancellationToken cancellationToken)
+    {
+        try
         {
-            _backupRepository = backupRepository;
-            _connectionString = configuration.GetConnectionString("Default") ?? "";
+            var backup = await _backupRepository.GetByIdAsync(request.Id, cancellationToken);
+
+            if (backup is null)
+            {
+                return Result<BackupEntity>.Invalid(new List<ValidationError> {
+                new () {ErrorMessage = "Backup not found",}
+            });
+            }
+
+            // temp path
+            var tempPath = Path.GetTempPath();
+            var pathToSave = Path.Combine(tempPath, backup.Name);
+
+            // download file
+            var file = await FileDownloadHelper.DownloadFileAsync(backup.Url, pathToSave);
+
+            if (file is false)
+            {
+                return Result<BackupEntity>.Invalid(new List<ValidationError> {
+                new () {ErrorMessage = "Backup not restored",}
+            });
+            }
+
+            // restore
+            await DatabaseHelper.RestoreDatabase(_connectionString, pathToSave);
+
+            // delete file
+            File.Delete(pathToSave);
+
+            return Result.Success(backup);
         }
-
-        public async Task<Result<BackupEntity>> Handle(RestoreBackupCommand request, CancellationToken cancellationToken)
+        catch (Exception ex)
         {
-            try
-            {
-                var backup = await _backupRepository.GetByIdAsync(request.Id, cancellationToken);
+            return Result.Error(ErrorHelper.GetExceptionError(ex));
 
-                if (backup is null)
-                {
-                    return Result<BackupEntity>.Invalid(new List<ValidationError> {
-                        new ValidationError { ErrorMessage = "Backup not found" }
-                    });
-                }
-
-                // Ruta de la carpeta C:\backups donde se encuentra el archivo de backup
-                var backupFolderPath = Path.Combine("C:", "backups");
-
-                // Ruta completa al archivo de backup
-                var pathToRestore = Path.Combine(backupFolderPath, backup.Name);
-
-                // Verificar si el archivo existe en la carpeta de backups
-                if (!File.Exists(pathToRestore))
-                {
-                    return Result<BackupEntity>.Invalid(new List<ValidationError> {
-                        new ValidationError { ErrorMessage = "Backup file not found" }
-                    });
-                }
-
-                // Restaurar la base de datos desde el archivo
-                await DatabaseHelper.RestoreDatabase(_connectionString, pathToRestore);
-
-                return Result.Success(backup);
-            }
-            catch (Exception ex)
-            {
-                return Result.Error(ErrorHelper.GetExceptionError(ex));
-            }
         }
     }
 }
